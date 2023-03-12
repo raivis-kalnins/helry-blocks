bb2a0f3db79a11ebcd813ddb39501e0043afc44e
Raivis Kalnins <raivis@63.lv>
2023-03-12 14:16:27 +0000

    setup

--- inc/editor-assets.php
+++ inc/editor-assets.php
@@ -5,9 +5,9 @@
  function dpwpblocks_editor_assets() {	
 	$name       = 'blocks';
 	$filepath   = 'dist/' . $name;
-	$asset_path = dpwpblocks_ROOT_PATH . $filepath . '.asset.php';
+	$asset_path = DPWPBLOCKS_ROOT_PATH . $filepath . '.asset.php';
 	$asset_file = file_exists( $asset_path ) ? include $asset_path : array( 'dependencies' => array(), 'version' => '1.0', );
-	$script_url = dpwpblocks_ROOT_URL . $filepath . '.js';
+	$script_url = DPWPBLOCKS_ROOT_URL . $filepath . '.js';
 	wp_register_script( 'dpwpblocks-editor', $script_url, array_merge( $asset_file['dependencies'], array( 'wp-api' ) ), $asset_file['version'], true );
 }
 add_action( 'init', 'dpwpblocks_editor_assets' );
@@ -18,10 +18,10 @@ add_action( 'init', 'dpwpblocks_editor_assets' );
 function wpdocs_enqueue_custom_public_style() {
 	$name       = 'public';
 	$filepath   = 'dist/' . $name;
-	$asset_path = dpwpblocks_ROOT_PATH . $filepath . '.asset.php';
+	$asset_path = DPWPBLOCKS_ROOT_PATH . $filepath . '.asset.php';
 	$asset_file = file_exists( $asset_path ) ? include $asset_path : array( 'dependencies' => array(), 'version' => '1.0', );
-	$script_url = dpwpblocks_ROOT_URL . 'dist/public.js';
-	$style_url = dpwpblocks_ROOT_URL . 'dist/public.scss.css';
+	$script_url = DPWPBLOCKS_ROOT_URL . 'dist/public.js';
+	$style_url = DPWPBLOCKS_ROOT_URL . 'dist/public.scss.css';
 	$v = $asset_file['version'];
 	wp_enqueue_style( 'public-dpwpblocks-css', $style_url, array(), $v, 'all' );
 	wp_enqueue_script( 'public-dpwpblocks-js', $script_url, array(), $v, 'all' );
@@ -34,10 +34,10 @@ add_action( 'wp_enqueue_scripts', 'wpdocs_enqueue_custom_public_style' );
 function wpdocs_enqueue_custom_admin_style() {
 	$name       = 'admin';
 	$filepath   = 'dist/' . $name;
-	$asset_path = dpwpblocks_ROOT_PATH . $filepath . '.asset.php';
+	$asset_path = DPWPBLOCKS_ROOT_PATH . $filepath . '.asset.php';
 	$asset_file = file_exists( $asset_path ) ? include $asset_path : array( 'dependencies' => array(), 'version' => '1.0', );
-	$script_url = dpwpblocks_ROOT_URL . 'dist/admin.js';
-	$style_url = dpwpblocks_ROOT_URL . 'dist/admin.scss.css';
+	$script_url = DPWPBLOCKS_ROOT_URL . 'dist/admin.js';
+	$style_url = DPWPBLOCKS_ROOT_URL . 'dist/admin.scss.css';
 	$v = $asset_file['version'];
 	wp_enqueue_style( 'admin-dpwpblocks-css', $style_url, array(), $v, 'all' );
 	wp_enqueue_script( 'admin-dpwpblocks-js', $script_url, array(), $v, 'all' );
